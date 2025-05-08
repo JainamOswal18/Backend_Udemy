ae63239bb07723b90f40ba57a0c6358f54479d7e
JainamOswal18 <jainamoswal1811@gmail.com>
2025-05-08 12:24:52 +0530

    completed basic ops project in node and postgres with error handling

--- 8.3 Travel Tracker/index.js
+++ 8.3 Travel Tracker/index.js
@@ -17,7 +17,7 @@ const pool = new Pool({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
+async function checkVisisted() {
   const results = await pool.query("SELECT country_code FROM visited_countries");
   let countries = [];
   results.rows.forEach((country) => {
@@ -25,7 +25,54 @@ app.get("/", async (req, res) => {
   });
   console.log(countries);
 
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
   res.render("index.ejs", { total: countries.length, countries: countries });
+});
+ 
+app.post("/add", async (req, res) => {
+  
+  try {
+    const country = req.body.country;
+
+    const results = await pool.query(
+      "SELECT country_code FROM countries WHERE country_name ILIKE '%' || $1 || '%'",
+      [country]
+    );
+    console.log(results.rows);
+
+    var country_code = results.rows[0].country_code;
+
+    try {
+      await pool.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [country_code]
+      );
+      res.redirect("/");
+    
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again!",
+      });
+    }
+
+  } catch (err) {
+    console.log(err);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again!"
+    });
+  }
+
 });
 
 app.listen(port, () => {
